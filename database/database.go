29dddaa1a7b281bc2138c1302e3d0167c8fddbfa
kennabila <kennabilas@gmail.com>
2017-09-15 22:50:46 +0700

    init database

---
+++ database/database.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"os"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Database struct {
+	Database *sql.DB
+}
+
+func NewDatabase() *MySql {
+	db, err := sql.Open("mysql", os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(127.0.0.1:3306)/"
+	checkErr(err)
+
+	return &MySql{db}
+}
+
+func InitDatabase(db *sql.DB) {
+   _,err = db.Exec("CREATE DATABASE " + os.Getenv("DB_NAME"))
+	checkErr(err)
+
+   _,err = db.Exec("USE " + os.Getenv("DB_NAME"))
+	checkErr(err)
+
+   _,err = db.Exec("CREATE TABLE users ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, telegram varchar(50), github varchar(50) )")
+	checkErr(err)
+}
+
+func checkErr(err *error) {
+	if err != nil {
+    panic(err)
+  }
+}
